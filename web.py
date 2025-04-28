6e0f8e5a3ad695c5b663b0868332f0da25b4fdda
Aiki <maximepag02@gmail.com>
2025-04-28 16:09:17 +0200

    done?

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.stats import pearsonr
+from pidog import Pidog
 
 app = Flask(__name__)
 
@@ -42,15 +43,27 @@ def matchcommand(command, actions, threshold=0.50):
     for id, document_tf_idf in enumerate(corpus):
         pearson_corr, _ = pearsonr(query_tf_idf, document_tf_idf)
         if pearson_corr > threshold:
-            result = {"ID": id, "document": texts[id+1], "similarity": float(pearson_corr)}
+            result = {"ID": id, "action": texts[id+1], "similarity": float(pearson_corr)}
             results.append(result)
     
-    return results.sort(key=lambda x: x['similarity'], reverse=True)
+    return results.sort(key=lambda x: x['similarity'], reverse=True) if results else None
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/command', methods=['POST'])
+def command():
+    command = request.json.get('command', '')
+    action = matchcommand(command, commands)
+    if action:
+        my_dog.do_action(action[0]['action'])
+        return action[0]['action']
+    return "No matching action found"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
-    
+    try:
+        my_dog = Pidog(head_init_angles=[0, 0, -30])
+        app.run(host='0.0.0.0',port=8080,debug=True)
+    finally:
+        my_dog.close()
