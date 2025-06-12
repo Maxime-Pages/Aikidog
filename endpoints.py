ba6fe88f60e80ed0de90818a5b68bb7f40108da0
Sanegv <sanegv5@gmail.com>
2025-06-12 16:22:34 +0200

    create requests

---
+++ endpoints.py
@@ -0,0 +1,52 @@
+import requests
+
+def analyse_sentiment(text):
+    url = "http://52.47.99.5:5000/predict"
+    payload = {"sentence": text}
+
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()  # Lève une erreur pour les codes de statut HTTP 4xx/5xx
+        data = response.json()
+
+        # Vérifie que la réponse contient bien la clé "sentiment"
+        if "sentiment" in data:
+            if data["sentiment"] == "NEGATIVE":
+                return 0
+            elif data["sentiment"] == "POSITIVE":
+                return 1
+        return None  # Si la clé "sentiment" est absente ou a une autre valeur
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error during request: {e}")
+        return None
+
+
+def search(text):
+    url = "http://52.47.99.5:8080/search"
+    payload = {"question": text}
+
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()  # Lève une erreur pour les codes de statut HTTP 4xx/5xx
+        data = response.json()
+
+        # Vérifie que la réponse contient bien la clé "sentiment"
+        if "answer" in data:
+            return data["answer"]
+        return None  # Si la clé "sentiment" est absente ou a une autre valeur
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error during request: {e}")
+        return None
+
+def main():
+    text = input(">")
+    sentiment = analyse_sentiment(text)
+    answer = search(text)
+    if sentiment is not None:
+        print(f'Emotion: {sentiment}')
+    if answer is not None:
+        print(f'Answer: {answer}')
+
+main()
