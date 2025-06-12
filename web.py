77fd0196535f986f0e6886a898e8ce775dc0b093
Aiki <maximepag02@gmail.com>
2025-06-12 16:16:19 +0200

    speech recognition

--- web.py
+++ web.py
@@ -3,10 +3,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.stats import pearsonr
 from pidog import Pidog
 from time import sleep
+import speechrecognition as sr
 
 app = Flask(__name__)
 my_dog = Pidog(head_init_angles=[0, 0, -30])
 sleep(1)
+amazon_url = "https://www.amazon.com/dp/B0C5Z3K6XG?ref=myi_title_dp"
 
 
 
@@ -69,6 +71,22 @@ def command():
         my_dog.do_action(action[0]['action'])
         return action[0]['action']
     return "No matching action found"
+
+@app.route('/listen')
+def listen():
+    recognizer = sr.Recognizer()
+    recognizer.energy_threshold = 4000
+    recognizer.dynamic_energy_threshold = True
+    recognizer.pause_threshold = 0.5
+    recognizer.non_speaking_duration = 0.5
+    #Listen to the mic
+    with sr.Microphone(chunk_size=8192) as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+        text = r.recognize_google(audio)
+        answer = getresponse(text)
+        return answer
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
